8462cbdd815819beb8d8079bfed4c00e354b9c22
Jérémie Roquet <arkanosis@gmail.com>
2009-12-04 14:53:48 +0100

    * Condorcet  - Improved sed scrapper  - Updated votes for Oversight

--- Condorcet/votes.sh
+++ Condorcet/votes.sh
@@ -8,4 +8,6 @@ if ((# < 2)); then
     exit 1
 fi
 
-wget "http://fr.wikipedia.org/wiki/$1" -O - 2> /dev/null | sed -n 's/&gt;/>/gp' | sed -n 's@<li>\([ 	[:upper:]=,>]\{2,\}\).*<a href="[^"]\+" title="\(Discussion \)\?[uU]tilisateur:\([^"]\+\)".\+@\1 | \3@p' >| $2
+wget "http://fr.wikipedia.org/wiki/$1" -O - 2> /dev/null |
+  sed -n 's/&gt;/>/gp' |
+  sed -n 's@<li>\(\([[:upper:]]\+[ 	]*[=,>][ 	]*\)\+[[:upper:]]\+\).*<a href="[^"]\+" title="\(Discussion \)\?[uU]tilisateur:\([^/"]\+\)".\+@\1 | \4@p' >| $2
