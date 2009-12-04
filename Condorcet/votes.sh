4e424f914b569e47102a9c2c29271cc23153c1ce
Jérémie Roquet <arkanosis@gmail.com>
2009-12-04 15:29:52 +0100

    * Condorcet  - Better sed scrapping output

--- Condorcet/votes.sh
+++ Condorcet/votes.sh
@@ -10,4 +10,5 @@ fi
 
 wget "http://fr.wikipedia.org/wiki/$1" -O - 2> /dev/null |
   sed -n 's/&gt;/>/gp' |
-  sed -n 's@<li>\(\([[:upper:]]\+[ 	]*[=,>][ 	]*\)\+[[:upper:]]\+\).*<a href="[^"]\+" title="\(Discussion \)\?[uU]tilisateur:\([^/"]\+\)".\+@\1 | \4@p' >| $2
+  sed -n 's@<li>\(\([[:upper:]]\+[ 	]*[=,>][ 	]*\)\+[[:upper:]]\+\).*<a href="[^"]\+" title="\(Discussion \)\?[uU]tilisateur:\([^/"]\+\)".\+@\1|\4@p' |
+  sed 's/,/=/g ; s/[ 	]//g ; s/[>=|]/ & /g' >| $2
