7f014f3c317bd5b8a275a8a3a10f932e9fe4775e
Jérémie Roquet <arkanosis@gmail.com>
2010-10-22 19:12:32 +0200

    * Utilities  - boxConv.py, script to convert old infoboxes with hiddenStructure to modular ones

---
+++ utilities/boxConv.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+# (C) 2010 Arkanosis
+# arkanosis@gmail.com
+
+import re
+
+_var = re.compile(r'(\{{3}.*?\|?\}{3}|\'{3}.*?\'{3})')
+_numbered = re.compile(r'(.*?)(?:\d*| actuel(?:le)?)$')
+
+with open('1erministre.tpl') as inputFile:
+    last = None
+    for line in inputFile:
+        for var in re.findall(_var, line):
+            current = var[3:-3]
+            if current[-1] == '|':
+                current = current[:-1]
+            if current != last:
+                if var[0] == '{':
+                    match = _numbered.match(current)
+                    entry = match.group(1)
+                    entry = entry[0].upper() + entry[1:]
+                    print '{{Infobox/Ligne mixte optionnel|%s|{{{%s}}}}}' % (entry, current)
+                else:
+                    print '{{Infobox/Sous-titre|%s}}' % current
+                last = current
+
+#{{Infobox/Sous-titre|Sous-titre 1}}
+#{{Infobox/Notice|Infobox Pays}}
