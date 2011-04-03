ba4b119eb7b5e9fd406e266c1bdb915ac1a3b202
Jérémie Roquet <arkanosis@gmail.com>
2011-04-03 21:06:30 +0200

    * Arkbot  - Copytraq -0.2: continuous, interactive monitoring; much more efficient than 0.1  - Added utils module with progress bar

--- Arkbot/copytraq.py
+++ Arkbot/copytraq.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-# CopyTraq v0.1
-# (C) 2010 Arkanosis
+# CopyTraq v0.2
+# (C) 2010-2011 Arkanosis
 # arkanosis@gmail.com
 
 # http://github.com/Arkanosis/Wikipedia/Arkbot
@@ -10,50 +10,37 @@
 # Ce script est mis à disposition sous licence MIT
 # http://www.opensource.org/licenses/mit-license.php
 
-# TODO accepter des dates du genre '5 minutes', '1 heure'... et des intervalles de temps
-
 import datetime
 import getpass
 import logging
 import subprocess
 import sys
-import termios
-import tty
 
 import arkbot
+import utils
 
 _webBrowser = 'firefox'
 _diffUrl = 'http://fr.wikipedia.org/w/index.php?title=%s&diff=%s&oldid=%s'
 
-def getch():
-	stdin = sys.stdin.fileno()
-	stdinAttr = termios.tcgetattr(stdin)
-	try:
-		tty.setraw(stdin)
-		c = sys.stdin.read(1)
-	finally:
-		termios.tcsetattr(stdin, termios.TCSADRAIN, stdinAttr)
-		return c
-
 if __name__ == '__main__':
-	print 'CopyTraq 0.1'
+	print 'CopyTraq 0.2'
 	print '(C) 2010 Arkanosis'
 	print 'arkanosis@gmail.com'
 	print
 
-	if '-publish' in sys.argv:
-		publish = True
-		sys.argv.remove('-publish')
+	if '-botflag' in sys.argv:
+		botflag = True
+		sys.argv.remove('-botflag')
 	else:
-		publish = False
-	if '-interactive' in sys.argv:
-		interactive = True
-		sys.argv.remove('-interactive')
-	else:
-		interactive = False
+		botflag = False
+
+	timestamp = '2000-01-01T00:00:00Z'
+	if len(sys.argv) == 3:
+		timestamp = sys.argv[-1]
+		sys.argv.remove(timestamp)
 
 	if len(sys.argv) != 2:
-		print 'Usage: votescrapper.py [-publish] [-interactive] <nbChanges>'
+		print 'Usage: copytraq.py [-botflag] <nbChanges> [timestamp]'
 		sys.exit(1)
 
 	date = datetime.datetime.now()
@@ -63,59 +50,61 @@ if __name__ == '__main__':
 
 	bot = arkbot.Arkbot(arkbot._botName, arkbot._wiki, logger)
 	try:
-		if publish or interactive:
+		if botflag:
 			bot.login(getpass.getpass('Bot password ? '))
 
 		print 'Keys:'
 		print ' s: show diff'
-		print ' p: pass (default)'
+		print ' p: pass'
+		print ' r: restart'
 		print ' q: quit'
 		print
 
-		revisions = []
-		for change in bot.recent(rcnamespace=0, rcshow='anon|!redirect|!bot', rclimit=sys.argv[1], rcprop='title|ids|sizes|user'): # !patrolled
-			if change.newlen - change.oldlen > 500:
-				added, _ = bot.diff(change.title.encode('utf8'), change.old_revid, change.revid)
-				if added:
-					longestAddition = max(len(add) for add in added)
-					if longestAddition > 1000:
-						diffUrl = _diffUrl % (change.title, change.revid, change.old_revid)
-						revisions.append((longestAddition, diffUrl, change.user))
+		while True:
+			progress = utils.ProgressBar(int(sys.argv[1]))
+			print
+			progress.update(0, '[0]')
 
-		for revision in revisions:
-			print revision[0], revision[1], revision[2], ' [s/P/q] ',
-			action = getch()
+			changes = []
+			for changeId, change in enumerate(bot.recent(rcnamespace=0, rcshow='anon|!redirect|!bot', rclimit=sys.argv[1], rcprop='title|ids|sizes|user|timestamp', rcend=timestamp)): # !patrolled
+				if changeId == 0:
+					timestamp = change.timestamp
+				if change.newlen - change.oldlen > 500:
+					added, _ = bot.diff(change.title.encode('utf8'), change.old_revid, change.revid)
+					if added:
+						longestAddition = max(len(add) for add in added)
+						if longestAddition > 300:
+							changes.append(change)
+				progress.update(changeId + 1, '[%d] %s' % (len(changes), utils.fancyTime(change.timestamp)))
+
+			print
+			print 'Last timestamp is', timestamp
+			print
+
+			for changeId, change in enumerate(changes):
+				print '[%d / %d] %s | %s | %s (+%s) [s/p/q] ' % (changeId + 1, len(changes), utils.fancyTime(change.timestamp), change.user, change.title, change.newlen - change.oldlen),
+				while True:
+					action = utils.getch()
+					if action == 's':
+						subprocess.call([_webBrowser, _diffUrl % (change.title, change.revid, change.old_revid)])
+					elif action == 'q':
+						sys.exit(0)
+					elif action != 'p':
+						continue
+					print
+					break
+
+			print 'Restart? [r/q] ',
 			while True:
-				if action == 's':
-					subprocess.call([_webBrowser, revision[1]])
+				action = utils.getch()
+				if action == 'r':
+					print
+					break
 				elif action == 'q':
 					sys.exit(0)
-				elif action != 'p':
-					continue
-				print
-				break
 
-		res = ''
-		# TODO Iterate on recent changes
-		# Detect potential copyvio
-		# Look for parts against some search engine
-		# Discard potential sources that are known mirrord
-		# Discard potential sources that refer to Wikipedia
-		# Build a report with
-		#  - link to the original
-		#  - link to the addition
-		#  - link to revert the addition
-		#  - link to ask for an history cleanup
-
-		# Interactive mode: open the potential sources in a web browser, revert automatically, and ask for an history cleanup automatically too
-
-		if publish:
-			bot.append('Discussion_utilisateur:' + arkbot._botName, res + 'Machinalement' + arkbot._signature % (date.day, arkbot._monthName[date.month - 1], date.year, date.hour, date.minute), 'Rapport de violations possibles de droits d\'auteur')
+		if botflag:
 			bot.logout()
-		elif interactive:
-			bot.logout()
-		else:
-			print res
 
 	except (arkbot.ArkbotException), e:
 		print e
---
+++ Arkbot/utils.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import sys
+import termios
+import tty
+
+class ProgressBar(object):
+    __width = 80
+
+    def __init__(self, size):
+        self.__size = size
+
+    def update(self, progression, text=''):
+        ratio = float(progression) / self.__size
+        percentage = int(100 * ratio)
+        barSize = int(ProgressBar.__width * ratio)
+        print chr(27) + '[A%3d%%[' % percentage + '=' * barSize  + '>' + ' ' * (ProgressBar.__width - barSize) + '] %d / %d' % (progression, self.__size), text
+
+def getch():
+	stdin = sys.stdin.fileno()
+	stdinAttr = termios.tcgetattr(stdin)
+	try:
+		tty.setraw(stdin)
+		c = sys.stdin.read(1)
+	finally:
+		termios.tcsetattr(stdin, termios.TCSADRAIN, stdinAttr)
+		return c
+
+def fancyTime(timestamp):
+	return timestamp[:-4].replace('T', ' ')
