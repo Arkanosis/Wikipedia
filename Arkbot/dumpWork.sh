a90248a0551b1cb3bf33f7b174111142ee345d4c
Jérémie Roquet <arkanosis@gmail.com>
2011-11-05 19:57:58 +0100

    * DumpWork  - Show some statistics before doing the stuff

--- Arkbot/dumpWork.sh
+++ Arkbot/dumpWork.sh
@@ -6,9 +6,17 @@ if ((# < 1)); then
 fi
 
 debug= #-debug
+
+export LANG=fr_FR.UTF-8
 dump=`date -d $1 '+%-d %B %Y'`
 
+echo Pages en impasse: `wc -l data/pagesEnImpasse-$1.txt | cut -d ' ' -f 1`
+echo Pages vides: `wc -l data/pagesVides-$1.txt | cut -d ' ' -f 1`
+noportal=`wc -l data/articlesSansPortail-full-$1.txt | cut -d ' ' -f 1`
+echo Articles sans portail: $noportal \($(((noportal+494)/495)) pages\)
+
 python post.py $debug -dump $dump -mode 1 data/pagesEnImpasse-$1.txt
+
 python post.py $debug -dump $dump -mode 2 data/pagesVides-$1.txt
 
 python noportal.py $debug -dump $dump -mode 4 data/lastEdit-$1.txt
