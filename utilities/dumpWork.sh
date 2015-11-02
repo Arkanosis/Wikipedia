15056acf6317f0fbb5b405b4b59edb50fac386a9
Jérémie Roquet <jroquet@arkanosis.net>
2015-11-02 16:14:00 +0100

    Update dumpWork.sh to the currently used version

--- utilities/dumpWork.sh
+++ utilities/dumpWork.sh
@@ -5,7 +5,7 @@ if ((# < 1)); then
   exit 1
 fi
 
-wget -q http://download.wikimedia.org/frwiki/$1/frwiki-$1-pages-articles.xml.bz2 -O - | bunzip2 > data/frwiki-$1.xml
+wget -q http://dumps.wikimedia.org/frwiki/$1/frwiki-$1-pages-articles.xml.bz2 -O - | bunzip2 > data/frwiki-$1.xml
 
 < data/frwiki-$1.xml awk -f pagesEnImpasse.awk | sort > data/pagesEnImpasse-$1.txt
 < data/frwiki-$1.xml awk -f emptyPages.awk | sort > data/pagesVides-$1.txt
@@ -15,4 +15,15 @@ wget -q http://download.wikimedia.org/frwiki/$1/frwiki-$1-pages-articles.xml.bz2
 < data/frwiki-$1.xml awk -f articlesSansPortail.awk | sort > data/articlesSansPortail-full-$1.txt
 sed -n 's/ !!! music//p' data/articlesSansPortail-full-$1.txt | sort > data/articlesSansPortail-musique-$1.txt
 sed -n 's/ !!! actor//p' data/articlesSansPortail-full-$1.txt | sort > data/articlesSansPortail-acteurs-$1.txt
-grep -v '\!\!\! \(music\|actor\)' data/articlesSansPortail-full-$1.txt | sort > data/articlesSansPortail-$1.txt
+sed -n 's/ !!! homo//p' data/articlesSansPortail-full-$1.txt | sort > data/articlesSansPortail-homo-$1.txt
+grep -v '\!\!\! \(music\|actor\|homo\)' data/articlesSansPortail-full-$1.txt | sort > data/articlesSansPortail-$1.txt
+
+< data/frwiki-$1.xml awk -f articlesSansInfobox.awk | sort > data/articlesSansInfobox-full-$1.txt
+sed -n 's/ !!! music//p' data/articlesSansInfobox-full-$1.txt | sort > data/articlesSansInfobox-musique-$1.txt
+sed -n 's/ !!! actor//p' data/articlesSansInfobox-full-$1.txt | sort > data/articlesSansInfobox-acteurs-$1.txt
+grep -v '\!\!\! \(music\|actor\)' data/articlesSansInfobox-full-$1.txt | sort > data/articlesSansInfobox-$1.txt
+
+#python noiw.py data/wikidatawiki-20130505.xml | sort -rn > data/articlesACreer-20130505.txt
+
+python commercials.py data/frwiki-$1.xml | grep -v '^\(Aide\|Fichier\|MediaWiki\|Modèle\|Portail\|Projet\|Wikipédia\):' | sort > data/frwiki-commercials-$1.txt
+python ns_redirects.py data/frwiki-$1.xml | sort > data/frwiki-ns_redirects-$1.txt
